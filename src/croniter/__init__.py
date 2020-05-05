1048053bfcbd9128eee7feb0d6af0b6b8ff0e98f
SaschaSchlemmer <35063695+SaschaSchlemmer@users.noreply.github.com>
2020-05-05 12:25:56 +0200

    Update __init__.py
    Support for importing `CroniterError` as a catch-all for errors raised by croniter.

--- src/croniter/__init__.py
+++ src/croniter/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 from .croniter import (
     croniter,
+    CroniterError,  # noqa
     CroniterBadDateError,  # noqa
     CroniterBadCronError,  # noqa
     CroniterNotAlphaError  # noqa
